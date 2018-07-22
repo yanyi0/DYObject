9a0414a24a77218d6f6a17c665b9b2b182be6575
yanyi0 <785144130@qq.com>
2018-07-23 00:16:22 +0800

    Initial commit

---
+++ Example/DYObject/DYViewController.m
@@ -0,0 +1,29 @@
+//
+//  DYViewController.m
+//  DYObject
+//
+//  Created by 785144130@qq.com on 07/23/2018.
+//  Copyright (c) 2018 785144130@qq.com. All rights reserved.
+//
+
+#import "DYViewController.h"
+
+@interface DYViewController ()
+
+@end
+
+@implementation DYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/DYObject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DYObject
+//
+//  Created by 785144130@qq.com on 07/23/2018.
+//  Copyright (c) 2018 785144130@qq.com. All rights reserved.
+//
+
+@import UIKit;
+#import "DYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DYAppDelegate class]));
+    }
+}
